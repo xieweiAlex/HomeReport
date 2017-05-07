d915a63d74bbd8cd9236628d606006561e25588d
xieweialex <xieweialex@163.com>
2017-05-07 13:03:40 -0700

    Initial Commit

---
+++ HomeReport/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  HomeReport
+//
+//  Created by 谢伟 on 07/05/2017.
+//  Copyright © 2017 MUM. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
