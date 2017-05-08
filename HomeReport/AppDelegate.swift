8f982d32cd017bf7d57e5267552988a35faad112
xieweialex <xieweialex@163.com>
2017-05-07 17:15:28 -0700

    fill datas from json to sqlite

--- HomeReport/AppDelegate.swift
+++ HomeReport/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        checkDataStore()
+        
+        let path = Bundle.main.resourcePath
+        print("bundle path is: \(path!)")
+        
         return true
     }
 
@@ -43,6 +50,138 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         coreData.saveContext()
     }
 
+    internal func checkDataStore() {
+        let request: NSFetchRequest<Home> = Home.fetchRequest()
+        
+        let moc = coreData.persistentContainer.viewContext
+        do {
+//            let homeCount = try moc.count(for: request)
+            
+//            guard homeCount != 0 else {
+//            if homeCount == 0 {
+            
+                uploadSampleData()
+//            }
+        }
+        catch {
+            fatalError("Error in counting home record ")
+        }
+    }
+    
+    internal func uploadSampleData() {
+        print("something here ")
+        let moc = coreData.persistentContainer.viewContext
+
+        let urlPath = Bundle.main.url(forResource: "homes", withExtension: "json")
+        let data = try? Data(contentsOf: urlPath!)
+        
+       print("urlpath: \(urlPath)")
+       print("data: \(data)")
+        
+        do {
+            let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+            
+            let jsonArray = jsonResult.value(forKey: "home") as! NSArray
+            print("jsonArray: \(jsonArray)")
+            
+            //var home: NSManagedObject?
+            for json in jsonArray {
+                let homeData = json as! [String: AnyObject]
+                
+                // Location
+                guard let city = homeData["city"] else {
+                    return
+                }
+                
+                // Price
+                guard let price = homeData["price"] else {
+                    return
+                }
+                
+                // Bed
+                guard let bed = homeData["bed"] else {
+                    return
+                }
+                
+                // Bath
+                guard let bath = homeData["bath"] else {
+                    return
+                }
+                
+                // Sqft
+                guard let sqft = homeData["sqft"] else {
+                    return
+                }
+                
+                // Picture
+                var image: UIImage?
+                if let currentImage = homeData["image"] {
+                    let imageName = currentImage as? String
+                    image = UIImage(named: imageName!)
+                }
+                
+                // Home type
+                guard let homeCategory = homeData["category"] else {
+                    return
+                }
+                let homeType = (homeCategory as! NSDictionary)["homeType"] as? String
+                
+                // Home status
+                guard let homeStatus = homeData["status"] else {
+                    return
+                }
+                let isForSale = (homeStatus as! NSDictionary)["isForSale"] as? Bool
+
+                // Home object initialization
+                let home: Home = homeType?.caseInsensitiveCompare("condo") == .orderedSame ? Condo(context: moc): SingleFamily(context: moc)
+//                home.price = price as! Double
+                home.price = price.doubleValue
+                home.bed = bed.int16Value
+                home.bed = bed.int16Value
+                home.bath = bath.int16Value
+                home.sqft = sqft.int16Value
+                home.image = NSData.init(data: UIImageJPEGRepresentation(image!, 1)!)
+                home.homeType = homeType
+                home.city = city as? String
+                home.isForSale = isForSale!
+
+                if let unitsPerBuilding = homeData["unitsPerBuilding"] {
+                    (home as! Condo).unitsPerBuilding = unitsPerBuilding.int16Value
+                }
+                
+                if let lotSize = homeData["lotSize"] {
+                    (home as! SingleFamily).lotSize = lotSize.int16Value
+                }
+
+                // Sale history
+                if let saleHistories = homeData["saleHistory"] {
+                    let saleHistoryData = home.saleHistory?.mutableCopy() as! NSMutableSet
+                    
+                    for saleDetail in saleHistories as! NSArray {
+                        let saleData = saleDetail as! [String: AnyObject]
+                        
+                        let saleHistory = SaleHistory(context: moc)
+                        saleHistory.soldPrice = saleData["soldPrice"] as! Double
+                        
+                        let soldDateStr = saleData["soldDate"] as! String
+                        let dateFormatter = DateFormatter()
+                        dateFormatter.dateFormat = "yyyy-MM-dd"
+                        let soldDate = dateFormatter.date(from: soldDateStr)
+                        saleHistory.soldDate = soldDate
+                        
+                        saleHistoryData.add(saleHistory)
+                        home.addToSaleHistory(saleHistoryData)
+                    }
+                }
+            }
+            
+            coreData.saveContext()
+        }
+        catch {
+            fatalError("Cannot upload sample data")
+        }
+        
+    }
 
 }
 
--- HomeReport/SaleHistory+CoreDataProperties.swift
+++ HomeReport/SaleHistory+CoreDataProperties.swift
@@ -9,15 +9,17 @@
 import Foundation
 import CoreData
 
-
 extension SaleHistory {
 
     @nonobjc public class func fetchRequest() -> NSFetchRequest<SaleHistory> {
         return NSFetchRequest<SaleHistory>(entityName: "SaleHistory")
     }
 
-    @NSManaged public var soldDate: NSDate?
+    @NSManaged public var soldDate: Date?
     @NSManaged public var soldPrice: Double
     @NSManaged public var home: Home?
 
 }
+
+
+
